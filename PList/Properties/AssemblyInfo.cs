e8f073201ba35367942dbf3ffb1345c895ff8dc7
mteper <michael.teper@elloco.com>
2015-07-05 03:22:51 -0700

    Added nuspec file and updated assemblyinfo version and description

--- PList/Properties/AssemblyInfo.cs
+++ PList/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PList")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".Net Library for working with Apple *.plist files.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("PList")]
 [assembly: AssemblyProduct("PList")]
-[assembly: AssemblyCopyright("Copyright ©2009")]
+[assembly: AssemblyCopyright("Copyright © 2009-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1.*")]
+[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
