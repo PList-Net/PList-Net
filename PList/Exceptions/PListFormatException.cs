bc2b9cba75d4f3cf38201f5e62122b6368f42f0f
mteper <michael.teper@elloco.com>
2015-07-14 11:35:12 -0700

    Formatting

--- PList/Exceptions/PListFormatException.cs
+++ PList/Exceptions/PListFormatException.cs
@@ -1,37 +1,47 @@
 ﻿using System;
 
-namespace PListNet.Exceptions {
+namespace PListNet.Exceptions
+{
 	/// <summary>
 	/// PListNet format exception.
 	/// </summary>
-    [global::System.Serializable]
-    public class PListFormatException : PListException {
+	[global::System.Serializable]
+	public class PListFormatException : PListException
+	{
 		/// <summary>
 		/// Initializes a new instance of the <see cref="PListNet.Exceptions.PListFormatException"/> class.
 		/// </summary>
-        public PListFormatException() { }
+		public PListFormatException()
+		{
+		}
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="PListNet.Exceptions.PListFormatException"/> class.
 		/// </summary>
 		/// <param name="message">Message.</param>
-		public PListFormatException(string message) : base(message) { }
+		public PListFormatException(string message) : base(message)
+		{
+		}
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="PListNet.Exceptions.PListFormatException"/> class.
 		/// </summary>
 		/// <param name="message">Message.</param>
 		/// <param name="inner">Inner.</param>
-		public PListFormatException(string message, Exception inner) : base(message, inner) { }
+		public PListFormatException(string message, Exception inner) : base(message, inner)
+		{
+		}
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="PListNet.Exceptions.PListFormatException"/> class.
 		/// </summary>
 		/// <param name="info">Info.</param>
 		/// <param name="context">Context.</param>
-        protected PListFormatException(
-          System.Runtime.Serialization.SerializationInfo info,
-          System.Runtime.Serialization.StreamingContext context)
-            : base(info, context) { }
-    }
+		protected PListFormatException(
+			System.Runtime.Serialization.SerializationInfo info,
+			System.Runtime.Serialization.StreamingContext context)
+			: base(info, context)
+		{
+		}
+	}
 }
